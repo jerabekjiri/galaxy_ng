f4773a7d37eaf3d018cfeb3b470500aa58481a7e
jerabekjiri <jerabekjirka@email.cz>
2024-11-20 21:54:39 +0100

    change path for galaxy token

--- galaxy_ng/tests/integration/utils/iqe_utils.py
+++ galaxy_ng/tests/integration/utils/iqe_utils.py
@@ -448,12 +448,20 @@ class AnsibleConfigFixture(dict):
 
         # workaround for a weird error with the galaxy cli lib ...
         # galaxy_token_fn = os.path.expanduser('~/.ansible/galaxy_token')
-        # galaxy_token_fn = os.path.expanduser(os.getenv('GALAXY_TOKEN_PATH', '~/.ansible/galaxy_token'))
-        # if not os.path.exists(os.path.dirname(galaxy_token_fn)):
-        #     os.makedirs(os.path.dirname(galaxy_token_fn))
-        # if not os.path.exists(galaxy_token_fn):
-        #     with open(galaxy_token_fn, 'w') as f:
-        #         f.write('')
+        # galaxy_token_fn = os.path.expanduser('~/.ansible/galaxy_token')
+        galaxy_token_fn = os.path.expanduser('.ansible/galaxy_token')
+        # galaxy_token_fn = os.path.dirname()
+        print('--------------galaxy token path-----------------------')
+
+
+
+        if not os.path.exists(os.path.dirname(galaxy_token_fn)):
+            print('--------------creating galxy token dir-----------------------')
+            os.makedirs(os.path.dirname(galaxy_token_fn))
+        if not os.path.exists(galaxy_token_fn):
+            with open(galaxy_token_fn, 'w') as f:
+                print('--------------writing empty galxytoken fn-----------------------')
+                f.write('')
 
     def __hash__(self):
         # To avoid TypeError: unhashable type: 'AnsibleConfigFixture'
